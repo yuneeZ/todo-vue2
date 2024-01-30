82badbd30ca8576f977e8b99d494ad33db4d2ccf
조가윤 <yuna.j@bugs.co.kr>
2024-01-30 14:05:17 +0900

    refactor: addTodo 수정

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -36,15 +36,24 @@ export default {
     };
   },
   methods: {
+    // addTodo() {
+    //   if (this.newTodoItem.trim() !== "") {
+    //     // 텍스트 앞 뒤 공백 문자열 제거
+    //     var value = this.newTodoItem && this.newTodoItem.trim();
+    //     // App으로 이벤트 전달
+    //     // localStorage.setItem(value, value);
+    //     this.$emit("addTodo", value);
+    //   } else {
+    //     this.showModal = !this.showModal;
+    //   }
+    //   this.clearInput();
+    // },
     addTodo() {
-      if (this.newTodoItem.trim() !== "") {
-        // 텍스트 앞 뒤 공백 문자열 제거
-        var value = this.newTodoItem && this.newTodoItem.trim();
-        // App으로 이벤트 전달
-        // localStorage.setItem(value, value);
-        this.$emit("addTodo", value);
+      const trimmedValue = this.newTodoItem.trim();
+      if (trimmedValue) {
+        this.$emit("addTodo", trimmedValue);
       } else {
-        this.showModal = !this.showModal;
+        this.showModal = true;
       }
       this.clearInput();
     },
